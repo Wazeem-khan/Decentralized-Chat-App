62622ccb456aaac02de5503fd44d0c9157e4b2c8
Phyo Pyae Sone Han <phyohan1234@gmail.com>
2023-02-19 01:11:32 +0630

    Added Files

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Home from './Home'
-import App from './App'
-import './index.css'
-import { BrowserRouter, Routes ,Route } from 'react-router-dom'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { AppProvider } from "./custom_hooks/context";
+import AppRoutes from "./routes/Routes";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/app" element={<App/>} />
-            </Routes> 
-        </BrowserRouter>
-    </React.StrictMode>
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  // <React.StrictMode>
+  <BrowserRouter>
+    <AppProvider>
+      <AppRoutes />
+    </AppProvider>
+  </BrowserRouter>
+  // </React.StrictMode>
+);
